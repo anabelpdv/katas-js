653ce3d827c937219b43a12d8ad9cdba42ea98c8
Anabel Perez <anabelpdv@gmail.com>
2019-08-18 23:31:36 -0400

    Add difference volumes cuboids kata

---
+++ level-8/difference-volume-cuboids.js
@@ -0,0 +1,24 @@
+/****************************************************************************************
+ * https: https://www.codewars.com/kata/58cb43f4256836ed95000f97
+ * Task:
+ * In this simple exercise, you will create a program that will take two lists of integers, 
+ * a and b. Each list will consist of 3 positive integers above 0, representing the dimensions 
+ * of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+ * For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the 
+ * volume of b is 20. Therefore, the function should return 8.
+ * Your function will be tested with pre-made examples as well as random ones.
+ * If you can, try writing it in one line of code.
+/*****************************************************************************************/
+
+
+/*************************************** my solution *************************************/
+function findDifference(a, b) {
+ return Math.abs(a.reduce((t,c)=>t*c) - b.reduce((t,c)=>t*c))
+}
+
+
+
+/*********************************** the best solution ***********************************/
+function find_difference(a, b) {
+  return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
+}
